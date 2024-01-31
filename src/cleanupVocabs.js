a965f85b27bb3591b2a27f646371b6c563895005
@s.roertgen <sroertgen@gmail.com>
2024-01-31 17:16:20 +0100

    Improve logging and add npm scripts

--- src/cleanupVocabs.js
+++ src/cleanupVocabs.js
@@ -17,42 +17,71 @@ const buildFilePathsForBuildLog = (buildInfo) => {
   return "dist/build/" + buildInfo.id + ".json"
 }
 
+const cleanupVocabularies = async (dry = false) => {
+  if (dry) {
+    console.log("Dry run, won't delete anything.")
+  }
+  const buildInfo = await readBuildDir()
+  const sortedBuildInfo = sortBuildInfo(buildInfo)
+  const branchesExisting = await Promise.all(sortedBuildInfo.map(b => checkIfBranchExists(b.repository, b.ref)))
+  const branchesNotExisting = sortedBuildInfo.filter((_, i) => !branchesExisting[i])
+  // console.log(branchesNotExisting)
+  // build paths to be removed
+  const filePathsToBeDeleted = branchesNotExisting.map(buildFilePaths)
+  console.log("Will delete ", filePathsToBeDeleted.length, "vocabulary branches.")
+  console.log("Will delete these paths (vocabularies): \n", filePathsToBeDeleted)
+  const fileErrors = []
+  if (!dry) {
+    // rm paths
+    await Promise.all(filePathsToBeDeleted.map(async (p) => {
+      try {
+        await fs.rm(p, { recursive: true, force: false })
+      } catch (error) {
+        console.log("caught")
+        console.error(error)
+        fileErrors.push([[p], error])
+      }
+    }))
+  }
+  // rm build logs of branches that are no longer existing
+  // find all build logs with repository & ref == branchesNotExisting
+  const buildInfoToBeDeleted = buildInfo
+    .filter(b => branchesNotExisting.
+      some(e => (e.repository === b.repository && e.ref === b.ref)))
+  const buildInfoFilePathsToBeDeleted = buildInfoToBeDeleted.map(buildFilePathsForBuildLog)
+  console.log("Will delete ", buildInfoToBeDeleted.length, "build logs.")
+  console.log("Will delete these paths (build logs): \n", buildInfoFilePathsToBeDeleted)
+  // rm that logs
+  if (!dry) {
+    await Promise.all(buildInfoFilePathsToBeDeleted.map(async (p) => {
+      try {
+        await fs.rm(p, { recursive: true, force: false })
+      } catch (error) {
+        console.log("caught")
+        console.error(error)
+        fileErrors.push([[p], error])
+      }
+    })
+    )
+  }
+  if (fileErrors.length) {
+    console.log("Encoutered errors deleting:")
+    fileErrors.forEach(e => {
+      console.log(`Path: ${e[0]}, error: ${e[1]}`)
+    })
+  }
+}
+
 const main = async () => {
   if (args.length) {
-    // TODO make a dry run option
+    console.log(args[0])
+    if (args[0] === "--dry") {
+      cleanupVocabularies(true)
+    } else {
+      throw Error("Invalid option. Pass --dry for dry run or nothing for a cleanup.")
+    }
   } else {
-    const buildInfo = await readBuildDir()
-    const sortedBuildInfo = sortBuildInfo(buildInfo)
-    const branchesExisting = await Promise.all(sortedBuildInfo.map(b => checkIfBranchExists(b.repository, b.ref)))
-    const branchesNotExisting = sortedBuildInfo.filter((_, i) => !branchesExisting[i])
-    // console.log(branchesNotExisting)
-    // build paths to be removed
-    const filePathsToBeDeleted = branchesNotExisting.map(buildFilePaths)
-    console.log(filePathsToBeDeleted)
-    // rm paths
-    filePathsToBeDeleted.forEach(p => {
-      try {
-        fs.rm(p, { recursive: true, force: true })
-      } catch (error) {
-        console.error(error)
-      }
-    })
-    // rm build logs of branches that are no longer existing
-    // find all build logs with repository & ref == branchesNotExisting
-    const buildInfoToBeDeleted = buildInfo
-      .filter(b => branchesNotExisting.
-        some(e => (e.repository === b.repository && e.ref === b.ref)))
-    console.log(buildInfoToBeDeleted.length)
-    // rm that logs
-    const buildInfoFilePathsToBeDeleted = buildInfoToBeDeleted.map(buildFilePathsForBuildLog)
-    console.log(buildInfoFilePathsToBeDeleted)
-    buildInfoFilePathsToBeDeleted.forEach(p => {
-      try {
-        fs.rm(p, { recursive: true, force: true })
-      } catch (error) {
-        console.error(error)
-      }
-    })
+    cleanupVocabularies(false)
   }
 }
 
